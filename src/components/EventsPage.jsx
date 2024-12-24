ee7798254b311d8d807f0ff4aa58b95289fc70f8
Yahia Alhejoj <yahiaalhejoj@Macmini.lan>
2024-12-23 20:56:24 -0800

    minor fixes

--- src/components/EventsPage.jsx
+++ src/components/EventsPage.jsx
@@ -54,7 +54,10 @@ const EventsPage = () => {
                     data-element_type="widget"
                   >
                     <div className="elementor-widget-container">
-                      <h2 className="elementor-heading-title elementor-size-default">
+                      <h2
+                        className="elementor-heading-title elementor-size-default"
+                        style={{ color: "#000" }}
+                      >
                         Upcoming Events
                       </h2>
                     </div>
@@ -82,12 +85,7 @@ const EventsPage = () => {
                     data-id="event-widget-1"
                     data-element_type="widget"
                   >
-                    <div className="elementor-widget-container">
-                      <h3 className="event-title">Event Title 1</h3>
-                      <p className="event-description">
-                        Brief description of the event goes here.
-                      </p>
-                    </div>
+                    <div className="elementor-widget-container"></div>
                   </div>
                 </div>
               </div>
@@ -103,100 +101,45 @@ const EventsPage = () => {
                     data-element_type="widget"
                   >
                     <div className="elementor-widget-container">
-                      <h3 className="event-title">Event Title 2</h3>
-                      <p className="event-description">
-                        Details for another upcoming event.
+                      <p
+                        className="event-description"
+                        style={{ color: "#000" }}
+                      >
+                        Guest Speakers: Sh. Yusha Evans, Sh. Ahmed Billoo,
+                        Hossein Behizad, and Hootan Afzali CrushRoar Activity
+                        Snacks & Food will be provided by Dennys (Halal)
+                        Families are welcome
                       </p>
-                      <p className="event-description">
-                        Details for another upcoming event.
-                      </p>
-                      <p className="event-description">
-                        Details for another upcoming event.
-                      </p>
-                      <p className="event-description">
-                        Details for another upcoming event.
-                      </p>
-                      <p className="event-description">
-                        Details for another upcoming event.
-                      </p>
-                      <p className="event-description">
-                        Details for another upcoming event.
-                      </p>
-                      <p className="event-description">
-                        Details for another upcoming event.
-                      </p>
-                      <p className="event-description">
-                        Details for another upcoming event.
-                      </p>
-                      <p className="event-description">
-                        Details for another upcoming event.
-                      </p>
-                      <p className="event-description">
-                        Details for another upcoming event.
-                      </p>
-                      <p className="event-description">
-                        Details for another upcoming event.
-                      </p>
-                      <p className="event-description">
-                        Details for another upcoming event.
-                      </p>
-                      <p className="event-description">
-                        Details for another upcoming event.
-                      </p>
-                      <p className="event-description">
-                        Details for another upcoming event.
-                      </p>
-                      <p className="event-description">
-                        Details for another upcoming event.
-                      </p>
-                      <p className="event-description">
-                        Details for another upcoming event.
-                      </p>
-                      <p className="event-description">
-                        Details for another upcoming event.
-                      </p>
-                      <p className="event-description">
-                        Details for another upcoming event.
-                      </p>
-                      <p className="event-description">
-                        Details for another upcoming event.
-                      </p>
-                      <p className="event-description">
-                        Details for another upcoming event.
-                      </p>
-                      <p className="event-description">
-                        Details for another upcoming event.
-                      </p>
-                      <p className="event-description">
-                        Details for another upcoming event.
-                      </p>
-                      <p className="event-description">
-                        Details for another upcoming event.
-                      </p>
-                      <p className="event-description">
-                        Details for another upcoming event.
-                      </p>
-                      <p className="event-description">
-                        Details for another upcoming event.
-                      </p>
-                      <p className="event-description">
-                        Details for another upcoming event.
-                      </p>
-                      <p className="event-description">
-                        Details for another upcoming event.
-                      </p>
-                      <p className="event-description">
-                        Details for another upcoming event.
-                      </p>
-                      <p className="event-description">
-                        Details for another upcoming event.
-                      </p>
-                      <p className="event-description">
-                        Details for another upcoming event.
-                      </p>
-                      <p className="event-description">
-                        Details for another upcoming event.
-                      </p>
+                      <h3 className="event-title" style={{ color: "#0078D4" }}>
+                        ICSGV Qiyam
+                      </h3>
+                      <button
+                        className="event-button"
+                        style={{
+                          backgroundColor: "#0078D4",
+                          color: "#fff",
+                          padding: "10px 20px",
+                          fontSize: "16px",
+                          border: "none",
+                          borderRadius: "5px",
+                          cursor: "pointer",
+                        }}
+                        onClick={() =>
+                          window.open(
+                            "https://zkqwdtgbb.cc.rs6.net/tn.jsp?f=001hnfTbaKh7u_V6-fApDQhdcl3djTsOpirjKj_D7NCHlOTU8JMAfHinNfYDs8LklgomXVW8sdOLJcPdpG2LtAKm4SQi8w6h3bUCVtogxENeYgnARI3H1zJHfFNTvlpGTnAskqMzMwbdAoT-hCIJCySxFPCxRBYfyBQzE7yTW_dE9w=&c=7qklSMyeBwu59oE9R55NlxpavG-AToz7uzy6zanAnX7prf_yJCJ5wg==&ch=G5qCy2td2Y1SIn8hNlXYDwAkttzTibw3MxtWD2ivj4u5M-dL8EIS0Q==",
+                            "_blank"
+                          )
+                        }
+                      >
+                        RSVP
+                      </button>
+
+                      <img
+                        src="https://ecp.yusercontent.com/mail?url=https%3A%2F%2Ffiles.constantcontact.com%2F9514b36a901%2Ffd3c3eee-ee93-4ffb-a9bf-76e3476af200.jpg&t=1735015727&ymreqid=8c3c2c0b-b2fa-a289-1c0d-310001017c00&sig=cKHnNIAOwl6cFcKbMLg51w--~D"
+                        alt="Event Image"
+                        className="event-image"
+                        style={{ maxWidth: "100%", height: "auto" }}
+                      />
                     </div>
                   </div>
                 </div>
