4db30028508dc56799acfcc586dbb66c8465f020
Yahia Alhejoj <yahiaalhejoj@Macmini.lan>
2025-02-07 08:34:12 -0800

    image optimize test 2

--- src/components/Library.jsx
+++ src/components/Library.jsx
@@ -36,7 +36,7 @@ const Library = () => {
           </div>
           <div style={{ flex: 1 }}>
             <img
-              src="https://github.com/Yahia89/icsgv/blob/icsgv/src/assets/shutterstock_1608535966.jpg?raw=true"
+              src="https://github.com/Yahia89/icsgv/blob/icsgv/src/assets/shutterstock_1608535966-min.jpg?raw=true"
               alt="Library"
               style={{
                 maxWidth: "100%",
