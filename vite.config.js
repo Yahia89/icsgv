a0345f07cd2671d86c99d6c2854cc178e1bf79a0
Yahia Alhejoj <yahiaalhejoj@Macmini.lan>
2025-02-08 03:45:29 -0800

    clarity added

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/icsgv/", // Changed from "/icsgv/" to "/"
   build: {
     outDir: "dist",
   },
