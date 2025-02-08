80df8de0536878f5bae206ba7460ac9fdb1bf01c
Yahia Alhejoj <yahiaalhejoj@Macmini.lan>
2025-02-08 01:26:50 -0800

    live

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { HelmetProvider } from 'react-helmet-async';
 import "./App.css";
 import Header from "./components/Header";
